35c5b4ff3fa6fa45b6065d187cde175534f8f403
ian-bravo <bravo.ian@gmail.com>
2023-08-14 14:40:13 -0700

    set up practice
    Co-authored-by: Paul LeTourneau <pletourneau62@gmail.com> Co-authored-by: Jonathan Lu <jdlu.coding@gmail.com> Co-authored-by: Jonathan Cheng <joncheng.dev@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+window.addEventListener("load", listIceCreamFlavors);
+
+function listIceCreamFlavors () {
+    const flavors = [];
+    flavors.push("strawberry", "vanilla", "chocolate");
+
+    let h1Element = document.querySelector("h1");
+    let ulElement = document.createElement("ul");
+
+    h1Element.after(ulElement);
+
+    flavors.forEach(function(flavor) {
+        let liElement = document.createElement("li");
+        liElement.append(flavor);
+        ulElement.append(liElement);
+    });
+    
+}
